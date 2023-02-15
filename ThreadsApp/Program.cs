67e0dc2256f4117e7b0b1fc589ed5f7169ac437b
student <fukatsummy@gmail.com>
2023-02-15 19:25:11 +0300

    DONE

--- ThreadsApp/Program.cs
+++ ThreadsApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading;
+using System.Text;
+using System.ComponentModel;
 
 
 
@@ -6,15 +8,18 @@ namespace ThreadsApp
 {
     internal class Program
     {
+       static ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(SaveDataToFile);
+       
+       
         static void Main(string[] args)
         {
-            MyObject myObject = new MyObject(Int32.Parse(Console.ReadLine()));
+            /*MyObject myObject = new MyObject(Int32.Parse(Console.ReadLine()));
             myObject.Start = Int32.Parse(Console.ReadLine());
             myObject.End = Int32.Parse(Console.ReadLine());
             ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(ConsoleWrite);
             /*ThreadStart threadStart = new ThreadStart(ConsoleWrite);//применяется к первой задаче
             Thread thread = new Thread(threadStart);*/
-            Thread thread = new Thread(parameterizedThreadStart);
+           /* Thread thread = new Thread(parameterizedThreadStart);
             for(int i=0; i<myObject.threads.Length;i++)
             {
                 myObject.threads[i] = new Thread(parameterizedThreadStart);
@@ -23,9 +28,9 @@ namespace ThreadsApp
             {
                 myObject.threads[i].Start(myObject);
                 myObject.threads[i].Join();
-            }
+            }*/
 
-            Random random= new Random();
+            Random random= new Random();//task 4
             int[] massive = new int[10000];
             for(int i=0; i<10000; i++)
             {
@@ -70,7 +75,7 @@ namespace ThreadsApp
             }
         }*/
 
-        static void ConsoleWrite(object my)
+        static void ConsoleWrite(object my) //task 1-2
         {
             int start = ((MyObject)my).Start;
             int end = ((MyObject)my).End;
@@ -81,7 +86,7 @@ namespace ThreadsApp
             }
             Console.WriteLine();
         }
-        static void TaskDelegate(object massive)
+        static void TaskDelegate(object massive)// task 4-5
         {
             ParameterizedThreadStart maxelemstart = new ParameterizedThreadStart(MaxElem);
             ParameterizedThreadStart minelemstart = new ParameterizedThreadStart(MinElem);
@@ -90,22 +95,56 @@ namespace ThreadsApp
             Thread thread2 = new Thread(minelemstart);
             Thread thread3 = new Thread(averageelemstart);
             thread1.Start(massive);
+            thread1.Join();
             thread2.Start(massive);
+            thread2.Join();
             thread3.Start(massive);
+            thread3.Join();
         }
-        static void MaxElem(object massive)
+        /*static void MaxElem(object massive)
         {
             
-                Console.WriteLine(((int[])massive).Max().ToString());
+            Console.WriteLine(((int[])massive).Max().ToString());
             Console.WriteLine("Это читерство");
+        }*/
+        static void MaxElem(object massive)
+        {
+            int result = ((int[])massive).Max();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStart);
+            thread.Start(result);
         }
-        static void MinElem(object massive)
+        /*static void MinElem(object massive)
         {
                 Console.WriteLine(((int[])massive).Min().ToString());
+        }*/
+        static void MinElem(object massive)
+        {
+             int result = ((int[])massive).Min();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStart);
+            thread.Start(result);
         }
-        static void AverageElem(object massive)
+        /*static void AverageElem(object massive)
         {
                 Console.WriteLine(((int[])massive).Average().ToString());
+        }*/
+        static void AverageElem(object massive)
+        {
+            double result = ((int[])massive).Average();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStart);
+            thread.Start(result);
+        }
+         static void SaveDataToFile(object number)
+        {
+            FileStreamOptions options= new FileStreamOptions();
+            options.Access = FileAccess.Write;
+            options.Share = FileShare.Write;
+            options.Mode = FileMode.OpenOrCreate;
+           StreamWriter writer = new StreamWriter("task5.txt", Encoding.UTF8, options);
+            writer.WriteLine(number.ToString());
+            writer.Close();
         }
 
     }
